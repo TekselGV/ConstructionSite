7856deba08d1feb60d72cd7181a10ab62b964ad1
borys-hvardionov <borys.hvardionov@realitycrisis.io>
2021-02-28 13:17:59 +0200

    Added player control

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -10,12 +10,21 @@ namespace CS.PlayerBehaviour
         private const string HORIZONTAL_AXIS = "Horizontal";
         private const string VERTICAL_AXIS = "Vertical";
 
+        private const float FINAL_DIRECTION_THRESHOLD = 0.1f;
+        /// <summary>
+        /// Defines minimal player rigidbody velocity to react with visual character rotation
+        /// </summary>
+        private const float MIN_VELOCITY_TO_ROTATE = 0.2f;
+
         [Header("Script references")]
         [SerializeField] private CameraController _cameraController;
         [SerializeField] private PlayerMovement _playerMovement;
+        [SerializeField] private Transform _visualParent;
         
         [Header("Options")]
         [SerializeField] private bool _lockCursor = true;
+        [Range(1f, 60f)]
+        [SerializeField] private float _playerRotationSpeed = 10f;
 
         #region PRIVATE_METHODS
         void Start()
@@ -28,6 +37,7 @@ namespace CS.PlayerBehaviour
             _cameraController.RotateCamera();
             SyncCameraControllerPosition();
             TrackMovementInputs();
+            RotateVisualParent();
         }
 
         private void UpdateCursorLockMode()
@@ -46,7 +56,41 @@ namespace CS.PlayerBehaviour
 
         private void TrackMovementInputs()
         {
+            // Since input values are changing from -1 to 1, we use this magic operation to remap from 0 to 1
+            float remappedHorizontalAxis = Input.GetAxisRaw(HORIZONTAL_AXIS) * 0.5f + 0.5f;
+            float remappedVerticalAxis = Input.GetAxisRaw(VERTICAL_AXIS) * 0.5f + 0.5f;
 
+            Vector3 horizontalLerpedDirection = Vector3.Lerp(-1 * _cameraController.transform.right, _cameraController.transform.right,
+                remappedHorizontalAxis);
+            Vector3 verticalLerpedDirection = Vector3.Lerp(-1 * _cameraController.transform.forward, _cameraController.transform.forward,
+                remappedVerticalAxis);
+
+            Vector3 finalDirection = horizontalLerpedDirection + verticalLerpedDirection;
+
+            // To avoid calling extra math calculations, we first check if the player even having some input currenctly
+            if (finalDirection.magnitude > FINAL_DIRECTION_THRESHOLD)
+            {
+                _playerMovement.IsAddingInputForce = true;
+                _playerMovement.InputForceVector = Vector3.ProjectOnPlane(finalDirection, Vector3.up);
+            }
+            else
+            {
+                _playerMovement.IsAddingInputForce = false;
+
+            }
+        }
+
+        /// <summary>
+        /// To rotate visual character around local vertical axis along with movement direction
+        /// </summary>
+        private void RotateVisualParent()
+        {
+            if (_playerMovement.VelocityRigidbody.magnitude > MIN_VELOCITY_TO_ROTATE)
+            {
+                Vector3 projectedVelocity = Vector3.ProjectOnPlane(_playerMovement.VelocityRigidbody, _playerMovement.transform.up);
+                Quaternion targetOrientation = Quaternion.LookRotation(projectedVelocity, _visualParent.up);
+                _visualParent.rotation = Quaternion.Slerp(_visualParent.rotation, targetOrientation, _playerRotationSpeed * Time.deltaTime);
+            }
         }
         #endregion
     }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,20 +7,35 @@ namespace CS.PlayerBehaviour
     [RequireComponent(typeof(Rigidbody))]
     public class PlayerMovement : MonoBehaviour
     {
+        private enum OrientationApproach
+        {
+            CenterOfMass,
+            Constraints
+        }
+        
         [Header("Main References")]
         [SerializeField] private Rigidbody _rigidbody;
 
         [Header("Options")]
-        
+
+        [Tooltip("What method of character orientation use;" +
+            "Center of mass - modify center of mass withoffset value trying to keep head up, more dynamic physical approach; " +
+            "Constraints - classic braindead way to constraint x/z rotation in rigidbody to always keep character vertical")]
+        [SerializeField] private OrientationApproach _orientationApproach = OrientationApproach.CenterOfMass;
+
         [Tooltip("Vertical offset of player's capsule Center Of Mass to stabilize it's orientation on the world" +
             "so he always tries to stay vertically. 0 - default center of mass position in the pivot point, " +
             "bigger value - better stabilization i.e. move to the local bottom")]
-        [Range(0f,3f)]
-        [SerializeField] private float _verticalCOMoffset = 1.5f;
+        [Range(0f,5f)]
+        [SerializeField] private float _verticalCOMoffset = 3f;
 
         [Tooltip("How strong we push our character's rigidbody when applying input force")]
-        [Range(0f,200f)]
+        [Range(0f,10000f)]
         [SerializeField] private float _inputForcePower = 100f;
+
+        [Tooltip("Define max player velocity in units/sec")]
+        [Range(0f, 14f)]
+        [SerializeField] private float _maxPlayerVelocity = 7f;
 
         /// <summary>
         /// Call to start adding input force
@@ -31,15 +46,32 @@ namespace CS.PlayerBehaviour
         /// </summary>
         public Vector3 InputForceVector { get; set; }
 
+        public Vector3 VelocityRigidbody => _rigidbody.velocity;
+
         #region PRIVATE METHODS
         private void Start()
         {
-            UpdateCenterOfMass();
+            UpdateOrientationApproach();
         }
 
         private void FixedUpdate()
         {
             CheckToAddForce();
+        }
+
+        private void UpdateOrientationApproach()
+        {
+            if (_orientationApproach == OrientationApproach.CenterOfMass)
+            {
+                UpdateCenterOfMass();
+                SetRigidbodyConstrains(false);
+
+            }
+            else
+            {
+                SetRigidbodyConstrains(true);
+                ResetRigidbodyCenterOfMass();
+            }
         }
 
         /// <summary>
@@ -50,14 +82,38 @@ namespace CS.PlayerBehaviour
             _rigidbody.centerOfMass = new Vector3(0f, -1 * _verticalCOMoffset, 0f);
         }
 
-        private void CheckToAddForce()
+        /// <summary>
+        /// Resets rigidbody center of mass to default position
+        /// </summary>
+        private void ResetRigidbodyCenterOfMass()
         {
-            if (IsAddingInputForce)
+            _rigidbody.ResetCenterOfMass();
+        }
+
+        /// <summary>
+        /// Turn's on or off all ridigbody constraints
+        /// </summary>
+        /// <param name="isConstrained">Set's whether constrains should be on or off</param>
+        private void SetRigidbodyConstrains(bool isConstrained)
+        {
+            if (isConstrained)
             {
-                _rigidbody.AddForce(InputForceVector * _inputForcePower, ForceMode.Force);
+                _rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+
+            }
+            else
+            {
+                _rigidbody.constraints = RigidbodyConstraints.None;
             }
         }
 
+        private void CheckToAddForce()
+        {
+            if (IsAddingInputForce && _rigidbody.velocity.magnitude < _maxPlayerVelocity)
+            {
+                _rigidbody.AddForceAtPosition(InputForceVector.normalized * _inputForcePower, transform.position, ForceMode.Force);
+            }
+        }
         #endregion
 
         #region PUBLIC_METHODS
